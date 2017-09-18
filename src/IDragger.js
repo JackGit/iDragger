27b184a24d0a08fd6846337e6f54475395090248
Jack Yang <ygjack414@hotmail.com>
2017-09-18 23:31:59 +0800

    update

--- src/IDragger.js
+++ src/IDragger.js
@@ -1,50 +1,18 @@
 // consider how to use FP
 
 // event: start, drag, stop
-//
-
-function bindEvent () {}
-
-function unbindEvent () {}
-
-client.addEventListener('mousemove', handler)
-displayObject.on('pointerdown', handler)
-
-function bindDragStart (handler) {
-  return this.client.on('mouvesss', handler)
-}
-
-var Utils = {
-  on: function () {},
-  off: function () {}
-}
+// startEvent: ['mousedown', 'touchstart'],
+// dragEvent: ['mousemove', 'touchmove'],
+// stopEvent: ['mouseup', 'touchend'],
 
 // var dragger = iDragger(options)
 // dragger(t1)
 // dragger(t2)
-function IDragger (client, options) {
-  var defaultOptions = {
-    startEvent: ['mousedown', 'touchstart'],
-    dragEvent: ['mousemove', 'touchmove'],
-    stopEvent: ['mouseup', 'touchend'],
-    addEvent: function () {},
-    removeEvent: function () {},
-    position: function () {},
-    getPosition: function () {}
+
+export default iDragger = function (options) {
+  return function (target) {
+    
   }
-
-  this.options.onDragStart
-  this.options.onDrag
-  this.options.onDragEnd
-}
-
-IDragger.prototype = {
-  constructor: IDragger,
-  _init: function () {},
-  _start: function () {},
-  _drag: function () {},
-  _stop: function () {},
-  destroy: function () {}
 }
 
 export default class IDragger {
@@ -61,32 +29,47 @@ export default class IDragger {
   destroy () {}
 }
 
-// proxy(client, options)
-IDragger.proxy = function (client, options = {
-  on: (eventName, handler) => client.addEventListener(eventName, handler),
-  off: (eventName, handler) => client.removeEventListener(eventName, handler)
-}) {
-  return {
-    client,
-    on: options.on,
-    off: options.off
-  }
-}
 
-// proxy(options)(client)
+const notImplemented = fn => { throw new Error(`${fn}() is not implemented`) }
+const domOn = function (eventName, handler) { this.addEventListener(eventName, handler) }
+const domOff = function (eventName, handler) { this.removeEventListener(eventName, handler) }
+
+/**
+ * const client = proxy({
+ *   on (eventName, handler) { this.addEventListener(eventName, handler) },
+ *   off (eventName, handler) { this.removeEventListener(eventName, handler )},
+ *   move (x, y) { this.style.top = y + 'px' this.style.left = x + 'px' },
+ *   position () { return { x: this.getBoundingClientRect().left, y: this.getBoundingClientRect().top } }
+ * })(client)
+ *
+ * client.on('mousemove', handler)
+ */
 function proxy (options = {}) {
-  const throwError = () => fn => { throw new Error(`proxy.${fn}() is not implemented`) }
   return function (client) {
     return {
       client,
       // bind event
-      on: options.on || throwError('on'),
+      on: options.on.bind(client) || notImplemented('on'),
       // unbind event
-      off: options.off || throwError('off'),
-      // move cliemove
-      move: options.move || throwError('move'),
-      // get position
-      position: options.position || throwError('position')
+      off: options.off.bind(client) || notImplemented('off'),
+      // move client
+      move: options.move.bind(client) || notImplemented('move'),
+      // get position of client
+      position: options.position.bind(client) || notImplemented('position')
     }
   }
 }
+
+
+export const positionProxy = proxy({
+  on: domOn,
+  off: domOff,
+  move (x, y) {},
+  position () {}
+})
+export const transformProxy = proxy({
+  on: domOn,
+  off: domOff,
+  move (x, y) {},
+  position () {}
+})
